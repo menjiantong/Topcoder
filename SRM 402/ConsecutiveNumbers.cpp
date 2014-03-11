52f795dff49795a8a7e8d430b5744ada1ef79bd8
nilmish <nilmish.iit@gmail.com>
2014-03-11 22:02:54 +0530

    SRM 402

---
+++ SRM 402/ConsecutiveNumbers.cpp
@@ -0,0 +1,189 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF                         (int)1e9
+#define EPS                         1e-9
+#define all(a)                      a.begin(), a.end()
+#define in(a,b)                     ( (b).find(a) != (b).end())
+#define pb                          push_back
+#define fill(a,v)                   memset(a, v, sizeof a)
+#define sz(a)                       ((int)(a.size()))
+#define mp                          make_pair
+#define f                           first
+#define sc                          second
+#define bitcount                    __builtin_popcount
+
+#define SSTR( x ) dynamic_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x ) ).str()
+#define SST(x) atoi(x)
+#define checkbit(n,b)                ( (n >> b) & 1)
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef pair<int,int> pp;
+typedef vector<string> VS;
+
+#define fr(i,s,n)    for(int i=s;i<(n);++i)
+#define MOD 1000000007
+class ConsecutiveNumbers {
+    public:
+    vector<int> missingNumber(vector<int> n) {
+
+    sort(all(n));
+
+    vector<int>a;
+    for(int i=1;i<n.size();++i)
+    {
+        int tmp=n[i]-n[i-1];
+        if(tmp>2 || tmp==0)
+        {
+            a.clear();
+            return a;
+        }
+        if(tmp==2)
+        {
+            if(a.size()>1)
+            {
+                a.clear();
+                return a;
+            }
+            a.pb(n[i]-1);
+        }
+    }
+
+    if(a.size()>1)
+    {
+        a.clear();
+        return a;
+    }
+
+    if(a.size()==0)
+    {
+        if(n[0]-1>=1)a.pb(n[0]-1);
+        a.pb(n[n.size()-1]+1);
+    }
+
+    return a;
+
+    }
+};
+
+// CUT begin
+ifstream data("ConsecutiveNumbers.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(vector<int> numbers, vector<int> __expected) {
+    time_t startClock = clock();
+    ConsecutiveNumbers *instance = new ConsecutiveNumbers();
+    vector<int> __result = instance->missingNumber(numbers);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> numbers;
+        from_stream(numbers);
+        next_line();
+        vector<int> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(numbers, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1394385206;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "ConsecutiveNumbers (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
