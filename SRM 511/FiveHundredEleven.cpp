ca02a6957b6f1dfab44be6fada468aa833e5cf23
nilmish <nilmish.iit@gmail.com>
2014-03-12 06:14:41 +0530

    SRM

---
+++ SRM 511/FiveHundredEleven.cpp
@@ -0,0 +1,191 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF                         (int)1e9
+#define EPS                         1e-9
+#define all(a)                      a.begin(), a.end()
+#define in(a,b)                     ( (b).find(a) != (b).end())
+#define pb                          push_back
+#define fill(a,v)                   memset(a, v, sizeof a)
+#define sz(a)                       ((int)(a.size()))
+#define mp                          make_pair
+#define f                           first
+#define sc                          second
+#define bitcount                    __builtin_popcount
+
+#define SSTR( x ) dynamic_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x ) ).str()
+#define SST(x) atoi(x)
+#define checkbit(n,b)                ( (n >> b) & 1)
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef pair<int,int> pp;
+typedef vector<string> VS;
+
+#define fr(i,s,n)    for(int i=s;i<(n);++i)
+#define MOD 1000000007
+
+VI c;
+int dp[100][1000];
+int f(int val,int mask)
+{
+    int& res=dp[val][mask];
+    if(res==-1)
+    {
+        if(mask==511)
+        {
+            res=1;
+        }
+        else
+        if(val==c.size())
+        {
+            res=0;
+        }
+        else
+        {
+            int cnt=0;
+            res=0;
+            for(int i=0;i<c.size();++i)
+            {
+                if((mask|c[i]) == mask)cnt++;
+                else
+                {
+                    int tmp=f(val+1,mask|c[i]);
+                    if(tmp==0)res=1;
+                }
+            }
+
+            if(cnt>val)
+            {
+                int tmp=f(val+1,mask);
+                if(tmp==0)res=1;
+            }
+        }
+    }
+
+    return res;
+}
+
+class FiveHundredEleven {
+    public:
+    string theWinner(vector<int> cards) {
+
+    c=cards;
+
+    fill(dp,-1);
+    int ans=f(0,0);
+    if(ans)return "Fox Ciel";
+    return "Toastman";
+        return "";
+    }
+};
+
+// CUT begin
+ifstream data("FiveHundredEleven.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> cards, string __expected) {
+    time_t startClock = clock();
+    FiveHundredEleven *instance = new FiveHundredEleven();
+    string __result = instance->theWinner(cards);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> cards;
+        from_stream(cards);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(cards, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1394571917;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "FiveHundredEleven (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
---
+++ SRM 511/Zoo.cpp
@@ -0,0 +1,174 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF                         (int)1e9
+#define EPS                         1e-9
+#define all(a)                      a.begin(), a.end()
+#define in(a,b)                     ( (b).find(a) != (b).end())
+#define pb                          push_back
+#define fill(a,v)                   memset(a, v, sizeof a)
+#define sz(a)                       ((int)(a.size()))
+#define mp                          make_pair
+#define f                           first
+#define sc                          second
+#define bitcount                    __builtin_popcount
+
+#define SSTR( x ) dynamic_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x ) ).str()
+#define SST(x) atoi(x)
+#define checkbit(n,b)                ( (n >> b) & 1)
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef pair<int,int> pp;
+typedef vector<string> VS;
+
+#define fr(i,s,n)    for(int i=s;i<(n);++i)
+#define MOD 1000000007
+class Zoo {
+    public:
+    long long theCount(vector<int> a) {
+
+    int dp[50];
+    fill(dp,0);
+
+    for(int i=0;i<a.size();++i)dp[a[i]]++;
+
+    if(dp[0]>2)return 0;
+    for(int i=1;i<50;++i)
+    {
+        if(dp[i]>dp[i-1])
+        {
+            return 0;
+        }
+    }
+
+    LL ans=1;
+    int ok=0;
+    for(int i=0;i<=40;++i)
+    {
+        if(dp[i]==1)
+        {
+            ok=1;
+            break;
+        }
+        if(dp[i]==2)ans*=2;
+        if(dp[i]==0)break;
+    }
+
+    if(ok)ans*=2;
+
+    return ans;
+
+        return 0;
+    }
+};
+
+// CUT begin
+ifstream data("Zoo.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> answers, long long __expected) {
+    time_t startClock = clock();
+    Zoo *instance = new Zoo();
+    long long __result = instance->theCount(answers);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> answers;
+        from_stream(answers);
+        next_line();
+        long long __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(answers, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1394571289;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "Zoo (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
