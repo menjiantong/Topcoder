46c47c17b6e29dc52f1a304a856fbe8cb6f996a5
nilmish <nilmish.iit@gmail.com>
2014-03-19 23:56:02 +0530

    SRM

---
+++ SRM 612/EmoticonsDiv1.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF                         (int)1e9
+#define EPS                         1e-9
+#define all(a)                      a.begin(), a.end()
+#define in(a,b)                     ( (b).find(a) != (b).end())
+#define pb                          push_back
+#define fill(a,v)                   memset(a, v, sizeof a)
+#define sz(a)                       ((int)(a.size()))
+#define mp                          make_pair
+#define f                           first
+#define sc                          second
+#define bitcount                    __builtin_popcount
+
+#define SSTR( x ) dynamic_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x ) ).str()
+#define SST(x) atoi(x)
+#define checkbit(n,b)                ( (n >> b) & 1)
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef pair<int,int> pp;
+typedef vector<string> VS;
+
+#define fr(i,s,n)    for(int i=s;i<(n);++i)
+#define MOD 1000000007
+
+int dp[1000][1000][30];
+int S;
+int ans=INF;
+int f(int s,int c,int val)
+{
+   // cout<<s<<" "<<c<<" "<<val<<endl;
+    int& res=dp[s][c][val];
+    if(res==-1)
+    {
+        if(s==S)
+        {
+            ans=min(ans,val);
+        }
+        else
+        {
+            if(val+1<30)f(s,s,val+1);
+            if(s>0 && val+1<30)f(s-1,c,val+1);
+            if(s+c<1000 && val+1<30)f(s+c,c,val+1);
+        }
+        res=1;
+    }
+}
+
+class EmoticonsDiv1 {
+    public:
+    int printSmiles(int smile) {
+
+    ans=INF;
+    S=smile;
+    fill(dp,-1);
+    f(1,0,0);
+    return ans;
+    }
+
+};
+
+// CUT begin
+ifstream data("EmoticonsDiv1.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int smiles, int __expected) {
+    time_t startClock = clock();
+    EmoticonsDiv1 *instance = new EmoticonsDiv1();
+    int __result = instance->printSmiles(smiles);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int smiles;
+        from_stream(smiles);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(smiles, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1394594299;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "EmoticonsDiv1 (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
